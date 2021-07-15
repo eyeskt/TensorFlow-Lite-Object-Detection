f922a10c65aa430d5719886e416e2d7c700c2d53
eyeskt <eyes.kt@gmail.com>
2021-07-15 17:45:43 +0800

    Update TFLite_detection_webcam.py
    for line#225 indent problem

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -222,7 +222,7 @@ while True:
             cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
 
             # Drag rectangle and text for the warning
-	    kids = "person"
+            kids = "person"
             if kids in label:
                 label = "Warning"
                 cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 0, 255), 2) # Draw rectangle
